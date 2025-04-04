ae1abc04cd39a629499ee0f5b6e8fcf6a79825ef
csLamar12 <90802465+csLamar12@users.noreply.github.com>
2025-04-04 18:22:04 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,130 @@
+import random
+import time
+
+
+def create_shingles(text, k=2):
+    # k variable represents our shingle size
+    if not text:
+        return []
+
+    result = []
+    for i in range(len(text) - 1):
+        result.append(text[i:i+2])
+    return result
+
+def create_vocabulary(*args):
+    vocabulary = {}
+    i = 0
+    if not args:
+        print("no arguments were passed")
+        return []
+
+    for arg in args:
+        if not isinstance(arg, list):
+            continue
+
+        for shingle in arg:
+            vocabulary.update({i: shingle})
+            i += 1
+    return vocabulary
+
+def create_vector(vocabulary, shingle_sequence):
+    vector_list = []
+    for shingle in vocabulary.values():
+        if shingle in shingle_sequence:
+            vector_list.append(1)
+        else:
+            vector_list.append(0)
+    return vector_list
+
+def calculate_jaccard_index(*vectors):
+    c11, c10, c01 = 0, 0, 0
+    counts_list = []
+    jaccard_index_list = []
+    if not vectors:
+        print("No vectors were passed")
+        return
+    for i in range(len(vectors)-1):
+        for j in range(len(vectors)-1):
+            if i == j + 1:  # Skip same vector comparison
+                continue
+            print("Comparing: " + "v" + str(i+1) + " and v" + str(j+2))
+            for x in range(len(vectors[i])):
+                if vectors[i][x] == vectors[j+1][x] and vectors[i][x] == 1:
+                    c11 += 1
+                elif vectors[i][x] != vectors[j+1][x] and vectors[i][x] == 1:
+                    c10 += 1
+                elif vectors[i][x] != vectors[j+1][x] and vectors[i][x] == 0:
+                    c01 += 1
+            if not c11 == c10 == c01 == 0:
+                counts_list.append([c11, c01, c10])
+            c11, c10, c01 = 0, 0, 0
+    for x11, y01, z10 in counts_list:
+        jaccard_index_list.append(round(x11/(x11+y01+z10),4))
+    return jaccard_index_list
+
+def create_permutations(vocabulary, p=4):
+    numbers = list(vocabulary.keys())
+    perm_list = []
+    for i in range(p):
+        temp_nums = numbers[:]
+        random.shuffle(temp_nums)
+        perm_list.append(temp_nums)
+    return perm_list
+
+def create_signatures(permutations, vocabulary, *vectors):
+    p = permutations
+    temp_signature = []
+    signature = []
+
+    for i in range(len(permutations)):
+        for j in range(len(vectors)):
+            print("Comparing: " + "p" + str(i+1) + " and v" + str(j+1))
+            for x in range(len(vectors[j])):
+                print("at " + str(vectors[j][p[i][x]]) + " x = " + str(x))
+                if vectors[j][p[i][x]] == 1:
+                    temp_signature.append(x)
+                    print("appended")
+                    break
+        signature.append(temp_signature[:])
+        temp_signature.clear()
+
+    return signature
+
+def get_vector_signatures(signature):
+    sig = []
+    temp_sig = []
+    print(len(signature[0]))
+    print(len(signature))
+    for j in range(len(signature[0])):
+        for i in range(len(signature)):
+            temp_sig.append(str(signature[i][j]))
+        sig.append(temp_sig[:])
+        temp_sig.clear()
+    return sig
+
+
+text1, text2, text3 = "bvxvmt", "brxvmt", "vvkxvb3"
+
+s1, s2, s3 = create_shingles(text1), create_shingles(text2), create_shingles(text3)
+
+vocab = create_vocabulary(s1, s2, s3)
+print("Vocabluary =", vocab)
+v1, v2, v3 = create_vector(vocab, s1), create_vector(vocab, s2), create_vector(vocab, s3)
+print("V1 =", v1)
+print("V2 =", v2)
+print("V3 =", v3)
+
+jaccard_index = calculate_jaccard_index(v1, v2, v3)
+
+print("Jaccard Index =", jaccard_index)
+permutations_list = create_permutations(vocab)
+print("Permutations =", permutations_list)
+signatures = create_signatures(permutations_list, vocab, v1,v2,v3)
+for sig in signatures:
+    print(sig)
+
+v_signatures = get_vector_signatures(signatures)
+for i in range(len(v_signatures)):
+    print("".join(v_signatures[i]))
+print(v_signatures)
